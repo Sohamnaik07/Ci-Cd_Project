6e0e278837ebfa10b5edae5efc83b845a1ccafb4
Sohamnaik07 <sohamnaik2019@gmail.com>
2026-06-08 00:27:27 +0530

    Soham

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Deployed via Docker + GitHub Actions + EC2</h1>');
+  res.send('<h1>Deployed via Docker + GitHub Actions + EC2  + Soham  </h1>');
 });
 
 app.listen(8080, () => console.log('App running on port 8080'));
